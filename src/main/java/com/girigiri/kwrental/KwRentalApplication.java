3cf2bd9815438399585a61f74d382943d2d92c76
yangdongjue5510 <djwhy5510@gmail.com>
2023-02-13 18:29:32 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/girigiri/kwrental/KwRentalApplication.java
@@ -0,0 +1,13 @@
+package com.girigiri.kwrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/girigiri/kwrental/KwRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girigiri.kwrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
