9937f311dd40a1efde0ec373cf367fb9bd083382
yangdongjue5510 <djwhy5510@gmail.com>
2023-02-15 20:09:26 +0900

    feat: 기자재 세부 내역 조회 API 구현 완료

---
+++ src/test/java/com/girigiri/kwrental/acceptance/EquipmentAcceptanceTest.java
@@ -0,0 +1,61 @@
+package com.girigiri.kwrental.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.girigiri.kwrental.support.DatabaseCleanUp;
+import com.girigiri.kwrental.equipment.Equipment;
+import com.girigiri.kwrental.equipment.EquipmentRepository;
+import com.girigiri.kwrental.equipment.dto.EquipmentResponse;
+import com.girigiri.kwrental.support.ResetDatabaseTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class EquipmentAcceptanceTest extends ResetDatabaseTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.execute();
+    }
+
+    @Test
+    @DisplayName("기자재 세부 내역 조회 API")
+    void getEquipment() {
+        // given
+        final Equipment equipment = TestFixtures.createEquipment();
+        equipmentRepository.save(equipment);
+
+        // when
+        final EquipmentResponse response = RestAssured.given()
+                .when().get("/api/equipments/{id}", equipment.getId())
+                .then().statusCode(HttpStatus.OK.value()).log().all()
+                .and().extract().as(EquipmentResponse.class);
+
+        // then
+        assertThat(response).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(TestFixtures.createEquipmentResponse());
+    }
+}
