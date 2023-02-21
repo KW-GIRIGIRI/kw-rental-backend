a3fe5fa54eea539750fb4bebbae15ba5eb3a7313
yangdongjue5510 <djwhy5510@gmail.com>
2023-02-21 19:02:22 +0900

    feat: 기자재 페이지 조회 API 구현

--- src/main/java/com/girigiri/kwrental/equipment/controller/EquipmentController.java
+++ src/main/java/com/girigiri/kwrental/equipment/controller/EquipmentController.java
@@ -1,24 +1,53 @@
 package com.girigiri.kwrental.equipment.controller;
 
 import com.girigiri.kwrental.equipment.dto.EquipmentDetailResponse;
+import com.girigiri.kwrental.equipment.dto.EquipmentResponse;
+import com.girigiri.kwrental.equipment.dto.EquipmentsPageResponse;
 import com.girigiri.kwrental.equipment.service.EquipmentService;
+import com.girigiri.kwrental.util.LinkUtils;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/equipments")
 public class EquipmentController {
 
     private final EquipmentService equipmentService;
+    private final LinkUtils linkUtils;
 
-    public EquipmentController(final EquipmentService equipmentService) {
+    public EquipmentController(final EquipmentService equipmentService, final LinkUtils linkUtils) {
         this.equipmentService = equipmentService;
+        this.linkUtils = linkUtils;
     }
 
     @GetMapping("/{id}")
     public EquipmentDetailResponse getEquipment(@PathVariable final Long id) {
         return equipmentService.findById(id);
+    }
+
+    @GetMapping
+    public EquipmentsPageResponse getEquipmentsPage(
+            @PageableDefault(sort = {"id"}, direction = Direction.DESC) Pageable pageable) {
+
+        final UriComponentsBuilder builder = MvcUriComponentsBuilder.fromMethodName(
+                EquipmentController.class, "getEquipmentsPage", pageable);
+
+        final Slice<EquipmentResponse> equipments = equipmentService.findEquipmentsBy(pageable);
+
+        String nextLink = linkUtils.createIfNextExists(equipments, builder);
+        String previousLink = linkUtils.createIfPreviousExists(equipments, builder);
+        return EquipmentsPageResponse.builder()
+                .nextLink(nextLink).previousLink(previousLink)
+                .page(pageable.getPageNumber())
+                .items(equipments.getContent())
+                .build();
     }
 }
--- src/test/java/com/girigiri/kwrental/equipment/service/EquipmentServiceTest.java
+++ src/test/java/com/girigiri/kwrental/equipment/service/EquipmentServiceTest.java
@@ -2,17 +2,22 @@ package com.girigiri.kwrental.equipment.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.girigiri.kwrental.acceptance.TestFixtures;
 import com.girigiri.kwrental.equipment.Equipment;
 import com.girigiri.kwrental.equipment.EquipmentRepository;
 import com.girigiri.kwrental.equipment.dto.EquipmentDetailResponse;
+import com.girigiri.kwrental.equipment.dto.EquipmentResponse;
 import com.girigiri.kwrental.equipment.exception.EquipmentNotFoundException;
 import com.girigiri.kwrental.support.ResetDatabaseTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 class EquipmentServiceTest extends ResetDatabaseTest {
@@ -34,7 +39,7 @@ class EquipmentServiceTest extends ResetDatabaseTest {
 
         // then
         assertThat(response).usingRecursiveComparison().ignoringFields("id")
-                .isEqualTo(TestFixtures.createEquipmentResponse());
+                .isEqualTo(TestFixtures.createEquipmentDetailResponse());
     }
 
     @Test
@@ -43,5 +48,26 @@ class EquipmentServiceTest extends ResetDatabaseTest {
         // given, when, then
         assertThatThrownBy(() -> equipmentService.findById(1L))
                 .isExactlyInstanceOf(EquipmentNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("등록된 기자재들을 페이지로 조회할 수 있다.")
+    void findEquipmentsBy() {
+        // given
+        equipmentRepository.save(TestFixtures.createEquipment());
+        equipmentRepository.save(TestFixtures.createEquipment());
+        equipmentRepository.save(TestFixtures.createEquipment());
+
+        // when
+        final Slice<EquipmentResponse> expect = equipmentService.findEquipmentsBy(
+                PageRequest.of(1, 1, Sort.by("id").descending()));
+
+        // then
+        assertAll(
+                () -> assertThat(expect.hasNext()).isTrue(),
+                () -> assertThat(expect.hasPrevious()).isTrue(),
+                () -> assertThat(expect.getContent()).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                        .containsExactly(TestFixtures.createEquipmentResponse())
+        );
     }
 }
