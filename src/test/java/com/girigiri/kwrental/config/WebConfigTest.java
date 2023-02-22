f2e770a16944e4651c0d7413af8facfdc7499d2e
yangdongjue5510 <djwhy5510@gmail.com>
2023-02-22 14:46:46 +0900

    feat: CORS 허용

---
+++ src/test/java/com/girigiri/kwrental/config/WebConfigTest.java
@@ -0,0 +1,40 @@
+package com.girigiri.kwrental.config;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.girigiri.kwrental.equipment.service.EquipmentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class WebConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EquipmentService equipmentService;
+
+    @Test
+    @DisplayName("URI가 /api/**인 요청에 CORS를 허용한다.")
+    void allowCors() throws Exception {
+        // given, when
+        mockMvc.perform(options("/api")
+                        .header("Access-Control-Request-Method", "GET")
+                        .header("Origin", "http://some-origin:1234"))
+
+                //then
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Methods",
+                        "GET,OPTIONS,POST,DELETE,PUT,HEAD,PATCH,TRACE"))
+                .andDo(print());
+    }
+}
