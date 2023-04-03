f19f932a51799bb3a7266f6d71e39a0bb0b6f819
yangdongjue5510 <djwhy5510@gmail.com>
2023-04-03 23:21:59 +0900

    feat: 기자재 수정 서비스 코드 구현

---
+++ src/main/java/com/girigiri/kwrental/equipment/dto/request/UpdateEquipmentRequest.java
@@ -0,0 +1,13 @@
+package com.girigiri.kwrental.equipment.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.Length;
+
+public record UpdateEquipmentRequest(
+        @NotEmpty String rentalDays, @NotEmpty String modelName, @NotEmpty String category,
+        @NotEmpty String maker, @NotEmpty String imgUrl, @Length(max = 200) String component,
+        @Length(max = 100) String purpose, @Length(max = 500) String description, @Positive Integer totalQuantity,
+        @Positive Integer maxRentalDays) {
+
+}
