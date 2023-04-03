b1d9e185acaac030976a9048188f05a0ab85d419
yangdongjue5510 <djwhy5510@gmail.com>
2023-04-03 13:52:13 +0900

    fix: cors allow header 추가

--- src/main/java/com/girigiri/kwrental/config/WebConfig.java
+++ src/main/java/com/girigiri/kwrental/config/WebConfig.java
@@ -1,17 +1,11 @@
 package com.girigiri.kwrental.config;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.HEAD;
-import static org.springframework.http.HttpMethod.OPTIONS;
-import static org.springframework.http.HttpMethod.PATCH;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.http.HttpMethod.TRACE;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -20,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedMethods(GET.name(), OPTIONS.name(), POST.name(), DELETE.name(),
-                        PUT.name(), HEAD.name(), PATCH.name(), TRACE.name());
+                        PUT.name(), HEAD.name(), PATCH.name(), TRACE.name())
+                .allowedHeaders(HttpHeaders.LOCATION, HttpHeaders.AUTHORIZATION);
     }
 }
