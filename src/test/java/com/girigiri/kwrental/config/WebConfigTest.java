bb2399a83f12b459a7dae68dca0be67bfc5d3050
yangdongjue5510 <djwhy5510@gmail.com>
2023-04-21 16:12:20 +0900

    fix: CORS 오리진 해결

--- src/test/java/com/girigiri/kwrental/config/WebConfigTest.java
+++ src/test/java/com/girigiri/kwrental/config/WebConfigTest.java
@@ -1,16 +1,16 @@
 package com.girigiri.kwrental.config;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,12 +25,12 @@ class WebConfigTest {
     void allowCors() throws Exception {
         // given, when
         mockMvc.perform(options("/api")
-                        .header("Access-Control-Request-Method", "GET")
-                        .header("Origin", "http://some-origin:1234"))
+                        .header("Access-Control-Request-Method", "PUT")
+                        .header("Origin", "http://localhost:3000"))
 
                 //then
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "http://localhost:3000"))
                 .andExpect(header().string("Access-Control-Allow-Methods",
                         "GET,OPTIONS,POST,DELETE,PUT,HEAD,PATCH,TRACE"))
                 .andDo(print());
