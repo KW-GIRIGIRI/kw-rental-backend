1730d02d236d94dcc903a2c7860ace3ecb9ed4b8
yangdongjue5510 <djwhy5510@gmail.com>
2023-04-25 16:10:40 +0900

    refactor: Fixture 이름 수정

--- src/test/java/com/girigiri/kwrental/testsupport/fixture/ReservationSpecFixture.java
+++ src/test/java/com/girigiri/kwrental/testsupport/fixture/ReservationSpecFixture.java
@@ -3,21 +3,20 @@ package com.girigiri.kwrental.testsupport.fixture;
 import com.girigiri.kwrental.equipment.domain.Equipment;
 import com.girigiri.kwrental.inventory.domain.RentalAmount;
 import com.girigiri.kwrental.inventory.domain.RentalPeriod;
-import com.girigiri.kwrental.reservation.domain.RentalSpec;
+import com.girigiri.kwrental.reservation.domain.ReservationSpec;
+import com.girigiri.kwrental.reservation.domain.ReservationSpec.ReservationSpecBuilder;
 
 import java.time.LocalDate;
 
-import static com.girigiri.kwrental.reservation.domain.RentalSpec.RentalSpecBuilder;
+public class ReservationSpecFixture {
 
-public class RentalSpecFixture {
-
-    public static RentalSpec create(final Equipment equipment) {
+    public static ReservationSpec create(final Equipment equipment) {
         return builder(equipment)
                 .build();
     }
 
-    public static RentalSpecBuilder builder(final Equipment equipment) {
-        return RentalSpec.builder()
+    public static ReservationSpecBuilder builder(final Equipment equipment) {
+        return ReservationSpec.builder()
                 .amount(new RentalAmount(1))
                 .period(new RentalPeriod(LocalDate.now().plusDays(1), LocalDate.now().plusDays(2)))
                 .equipment(equipment);
