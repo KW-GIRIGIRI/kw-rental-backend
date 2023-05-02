ae79d8ca2ea60e4d8f668452c5b7bf17ee2b1ce7
yangdongjue5510 <djwhy5510@gmail.com>
2023-05-02 18:27:15 +0900

    feat: 대여 중인 대여 상세를 조회하는 레포지토리 구현

--- src/test/java/com/girigiri/kwrental/rental/repository/RentalSpecRepositoryTest.java
+++ src/test/java/com/girigiri/kwrental/rental/repository/RentalSpecRepositoryTest.java
@@ -1,15 +1,23 @@
 package com.girigiri.kwrental.rental.repository;
 
 import com.girigiri.kwrental.config.JpaConfig;
+import com.girigiri.kwrental.equipment.domain.Equipment;
+import com.girigiri.kwrental.equipment.repository.EquipmentRepository;
 import com.girigiri.kwrental.rental.domain.RentalSpec;
+import com.girigiri.kwrental.reservation.domain.ReservationSpec;
+import com.girigiri.kwrental.reservation.repository.ReservationSpecRepository;
+import com.girigiri.kwrental.testsupport.fixture.EquipmentFixture;
 import com.girigiri.kwrental.testsupport.fixture.RentalSpecFixture;
+import com.girigiri.kwrental.testsupport.fixture.ReservationSpecFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -20,6 +28,10 @@ class RentalSpecRepositoryTest {
 
     @Autowired
     private RentalSpecRepository rentalSpecRepository;
+    @Autowired
+    private ReservationSpecRepository reservationSpecRepository;
+    @Autowired
+    private EquipmentRepository equipmentRepository;
 
     @Test
     @DisplayName("대여 상세를 모두 저장한다.")
@@ -38,5 +50,29 @@ class RentalSpecRepositoryTest {
                 () -> assertThat(rentalSpec1.getAcceptDateTime()).isNotNull(),
                 () -> assertThat(rentalSpec2.getAcceptDateTime()).isNotNull()
         );
+    }
+
+    @Test
+    @DisplayName("특정 기자재의 특정 날짜에 대여 중인 대여 상세를 조회한다.")
+    void findRentedRentalSpecs() {
+        // given
+        final LocalDateTime acceptTime = LocalDateTime.now();
+        final LocalDateTime returnTime = LocalDateTime.now();
+        final Equipment equipment1 = equipmentRepository.save(EquipmentFixture.builder().modelName("modelName1").build());
+        final Equipment equipment2 = equipmentRepository.save(EquipmentFixture.builder().modelName("modelName2").build());
+        final ReservationSpec reservationSpec1 = reservationSpecRepository.save(ReservationSpecFixture.builder(equipment1).build());
+        final ReservationSpec reservationSpec2 = reservationSpecRepository.save(ReservationSpecFixture.builder(equipment2).build());
+
+        final RentalSpec rentalSpec1 = RentalSpecFixture.builder().acceptDateTime(acceptTime).propertyNumber("11111111").reservationSpecId(reservationSpec1.getId()).build();
+        final RentalSpec rentalSpec2 = RentalSpecFixture.builder().acceptDateTime(acceptTime).returnDateTime(returnTime).propertyNumber("22222222").reservationSpecId(reservationSpec1.getId()).build();
+        final RentalSpec rentalSpec3 = RentalSpecFixture.builder().acceptDateTime(acceptTime).propertyNumber("33333333").reservationSpecId(reservationSpec2.getId()).build();
+
+        rentalSpecRepository.saveAll(List.of(rentalSpec1, rentalSpec2, rentalSpec3));
+
+        // when
+        final Set<RentalSpec> rentedRentalSpecs = rentalSpecRepository.findRentedRentalSpecs(equipment1.getId(), LocalDateTime.now());
+
+        // then
+        assertThat(rentedRentalSpecs).containsExactlyInAnyOrder(rentalSpec1);
     }
 }
