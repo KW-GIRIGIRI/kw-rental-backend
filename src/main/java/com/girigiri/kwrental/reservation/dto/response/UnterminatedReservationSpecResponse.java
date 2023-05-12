ebbe85aab5b7f4214adf6ab18e94daeb6d3e675a
yangdongjue5510 <djwhy5510@gmail.com>
2023-05-12 16:16:26 +0900

    fix: 현재 내 대여 예약 조회 API에 이미지 링크 추가

--- src/main/java/com/girigiri/kwrental/reservation/dto/response/UnterminatedReservationSpecResponse.java
+++ src/main/java/com/girigiri/kwrental/reservation/dto/response/UnterminatedReservationSpecResponse.java
@@ -13,16 +13,18 @@ public class UnterminatedReservationSpecResponse {
 
     private Category category;
     private String modelName;
+    private String imgUrl;
     private Integer rentalAmount;
     private ReservationSpecStatus status;
 
     private UnterminatedReservationSpecResponse() {
     }
 
-    private UnterminatedReservationSpecResponse(final Long id, final Category category, final String modelName, final Integer rentalAmount, final ReservationSpecStatus status) {
+    private UnterminatedReservationSpecResponse(final Long id, final Category category, final String modelName, final String imgUrl, final Integer rentalAmount, final ReservationSpecStatus status) {
         this.id = id;
         this.category = category;
         this.modelName = modelName;
+        this.imgUrl = imgUrl;
         this.rentalAmount = rentalAmount;
         this.status = status;
     }
@@ -30,6 +32,6 @@ public class UnterminatedReservationSpecResponse {
     public static UnterminatedReservationSpecResponse from(final ReservationSpec reservationSpec) {
         final Equipment equipment = reservationSpec.getEquipment();
         return new UnterminatedReservationSpecResponse(
-                reservationSpec.getId(), equipment.getCategory(), equipment.getModelName(), reservationSpec.getAmount().getAmount(), reservationSpec.getStatus());
+                reservationSpec.getId(), equipment.getCategory(), equipment.getModelName(), equipment.getImgUrl(), reservationSpec.getAmount().getAmount(), reservationSpec.getStatus());
     }
 }
