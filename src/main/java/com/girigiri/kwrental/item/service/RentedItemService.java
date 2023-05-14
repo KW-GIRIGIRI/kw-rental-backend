6e4288cc168aa6f3b5219c06420bcefd18e34a15
yangdongjue5510 <djwhy5510@gmail.com>
2023-05-14 17:25:14 +0900

    feat: 자산번호와 기간에 해당하는 대여 상태를 조회하는 서비스 메서드 구현

--- src/main/java/com/girigiri/kwrental/item/service/RentedItemService.java
+++ src/main/java/com/girigiri/kwrental/item/service/RentedItemService.java
@@ -1,9 +1,15 @@
 package com.girigiri.kwrental.item.service;
 
+import com.girigiri.kwrental.item.dto.response.RentalCountsDto;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Set;
 
 public interface RentedItemService {
 
     Set<String> getRentedPropertyNumbers(Long equipmentId, LocalDateTime date);
+
+    Map<String, RentalCountsDto> getRentalCountsByPropertyNumbersBetweenDate(Set<String> propertyNumbers, LocalDate from, LocalDate to);
 }
