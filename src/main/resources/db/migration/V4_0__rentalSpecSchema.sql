a7be827ab1a8565ddb8ec3e29b75c14978dd9a2e
yangdongjue5510 <djwhy5510@gmail.com>
2023-05-24 06:03:19 +0900

    config: 마이그레이션 코드 추가

--- src/main/resources/db/migration/V4_0__rentalSpecSchema.sql
+++ src/main/resources/db/migration/V4_0__rentalSpecSchema.sql
@@ -1,4 +1,4 @@
-alter table rental_spec property_number varchar (20) null;
+alter table rental_spec modify property_number varchar (20) null;
 ALTER TABLE rental_spec
     ADD COLUMN dtype VARCHAR(20) NOT NULL DEFAULT 'equipment';
 ALTER TABLE rental_spec
