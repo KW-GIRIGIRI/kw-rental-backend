5bfc2d8f3b8aa81d8b5f207b6356066b18840301
yangdongjue5510 <djwhy5510@gmail.com>
2023-06-11 16:39:49 +0900

    fix: 랩실을 특정 기간에 이미 대여한 경우 에러 메시지 재수정

--- src/main/java/com/girigiri/kwrental/reservation/exception/AlreadyReservedLabRoomException.java
+++ src/main/java/com/girigiri/kwrental/reservation/exception/AlreadyReservedLabRoomException.java
@@ -4,6 +4,6 @@ import com.girigiri.kwrental.common.exception.BadRequestException;
 
 public class AlreadyReservedLabRoomException extends BadRequestException {
 	public AlreadyReservedLabRoomException() {
-		super("해당 기간에 대여한 이력이 있습니다. 랩실은 기간 내 1번만 대여할 수 있습니다.");
+		super("랩실은 하루에 한 번만 대여할 수 있습니다.");
 	}
 }
