713d2167520233a44bbbbe00dbd389e044654de1
yangdongjue5510 <djwhy5510@gmail.com>
2023-06-27 17:42:36 +0900

    test: LabROomReservation 테스트 추가

--- src/main/java/com/girigiri/kwrental/reservation/domain/LabRoomReservation.java
+++ src/main/java/com/girigiri/kwrental/reservation/domain/LabRoomReservation.java
@@ -4,10 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.girigiri.kwrental.inventory.domain.RentalPeriod;
-import com.girigiri.kwrental.reservation.exception.IllegalRentDateException;
-import com.girigiri.kwrental.reservation.exception.LabRoomReservationNotRentedWhenReturnException;
-import com.girigiri.kwrental.reservation.exception.LabRoomReservationNotReservedWhenAcceptException;
-import com.girigiri.kwrental.reservation.exception.LabRoomReservationSpecNotOneException;
+import com.girigiri.kwrental.reservation.exception.LabRoomReservationException;
 
 import lombok.Getter;
 
@@ -19,22 +16,22 @@ public class LabRoomReservation {
 	public LabRoomReservation(final Reservation reservation) {
 		final List<ReservationSpec> specs = reservation.getReservationSpecs();
 		if (specs.size() != 1)
-			throw new LabRoomReservationSpecNotOneException();
+			throw new LabRoomReservationException("랩실 대여 예약 상세는 하나여야 합니다.");
 		this.reservation = reservation;
 	}
 
 	public void validateWhenRent() {
 		final List<ReservationSpec> specs = reservation.getReservationSpecs();
 		if (!specs.stream().allMatch(ReservationSpec::isReserved))
-			throw new LabRoomReservationNotReservedWhenAcceptException();
+			throw new LabRoomReservationException("랩실 대여를 하려는 예약 상세는 예약 상태여야 합니다.");
 		if (!specs.stream().allMatch(spec -> spec.containsDate(LocalDate.now())))
-			throw new IllegalRentDateException();
+			throw new LabRoomReservationException("대여 수령 날짜가 대여 신청 기간에 없습니다.");
 	}
 
 	public void validateWhenReturn() {
 		final List<ReservationSpec> specs = reservation.getReservationSpecs();
 		if (!specs.stream().allMatch(ReservationSpec::isRented))
-			throw new LabRoomReservationNotRentedWhenReturnException();
+			throw new LabRoomReservationException("반납하려는 대여 예약 상세가 대여 중 상태가 아닙니다.");
 	}
 
 	public Long getReservationSpecId() {
@@ -50,6 +47,7 @@ public class LabRoomReservation {
 	}
 
 	public void normalReturnAll() {
+		validateWhenReturn();
 		final List<ReservationSpec> specs = reservation.getReservationSpecs();
 		specs.forEach(spec -> spec.setStatus(ReservationSpecStatus.RETURNED));
 		reservation.updateIfTerminated();
--- src/main/java/com/girigiri/kwrental/reservation/exception/LabRoomReservationNotReservedWhenAcceptException.java
+++
@@ -1,9 +0,0 @@
-package com.girigiri.kwrental.reservation.exception;
-
-import com.girigiri.kwrental.common.exception.BadRequestException;
-
-public class LabRoomReservationNotReservedWhenAcceptException extends BadRequestException {
-    public LabRoomReservationNotReservedWhenAcceptException() {
-        super("랩실 대여를 하려는 예약 상세는 예약 상태여야 합니다.");
-    }
-}
