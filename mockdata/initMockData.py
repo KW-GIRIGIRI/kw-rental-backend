e47b3d53c57c247ae5bf2aba484fed936f5d4d86
yangdongjue5510 <djwhy5510@gmail.com>
2023-08-14 13:47:54 +0900

    feat: db connection 코드 구현

--- mockdata/initMockData.py
+++ mockdata/initMockData.py
@@ -1,11 +1,24 @@
 import yaml
 from peewee import *
 
-
 def load_conf_file(config_file):
     with open(config_file, "r") as config_file:
-        config = yaml.safe_load(config_file)
-        print(config)
+        return yaml.safe_load(config_file)
+    
+def parse_datasource(conf_file) :
+    return conf_file['database']
 
 
-load_conf_file('../src/main/resources/security/security-release.yaml')
+
+conf_file = load_conf_file('../src/main/resources/security/security-performance.yaml')
+datasource = parse_datasource(conf_file)
+
+db_url = datasource['url']
+username = datasource['username']
+password = datasource['password']
+
+mySqlDb = MySQLDatabase('performance_db', user=username, password=password, host=db_url, port=3306)
+
+
+mySqlDb.connect()
+mySqlDb.close()
